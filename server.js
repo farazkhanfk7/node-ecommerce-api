50cf1d4317deb5ec615c03da966b07ed60bab01b
farazkhanfk7 <farazkhan138@gmail.com>
2021-12-30 11:11:56 +0530

    routes init

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ const connectDB = require('./config/db')
 //connecting mongo
 connectDB();
 
+//Init middleware
+app.use(express.json())
+
 app.get('/', (req,res) => {
     res.send("API working")
 });
+
+app.use('/api/user', require('./routes/user'))
+app.use('/api/product', require('./routes/product'))
+app.use('/api/cart', require('./routes/cart'))
+app.use('/api/order', require('./routes/order'))
 
 const port = process.env.PORT || 3000
 
